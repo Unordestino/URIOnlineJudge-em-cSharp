3695db94c37062bbc378e71cdc63bfd17a0b8dc5
Davisilveira <80595329+Unordestino@users.noreply.github.com>
2021-09-01 14:13:22 -0300

    Add files via upload

---
+++ 1159.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1159 {
+    class Program {
+        static void Main(string[] args) {
+
+            int x = int.Parse(Console.ReadLine());
+            while (x != 0) {
+                int cont = 0;
+                int soma = 0;
+
+                while (cont < 5) {
+                    if (x % 2 == 0) {
+                        soma += x;
+                        cont++;
+                    }
+                    x++;
+                }
+                Console.WriteLine(soma);
+                x = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
+
