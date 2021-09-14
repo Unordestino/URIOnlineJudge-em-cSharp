90fffd2cb87b51414a319e31f0793d7bd6e65d90
Davisilveira <80595329+Unordestino@users.noreply.github.com>
2021-09-14 13:52:30 -0300

    Add files via upload

---
+++ 1178.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1178 {
+    class Program {
+        static void Main(string[] args) {
+
+            double X = double.Parse(Console.ReadLine());
+            double[] N = new double[100];
+
+            N[0] = X;
+
+            for (int i = 1; i < N.Length; i++) {
+                N[i] = N[i - 1] / 2.0000;
+            }
+
+            for (int i = 0; i < N.Length; i++) {
+                Console.WriteLine($"N[{i}] = {N[i].ToString("F4")}");
+            }
+
+        }
+    }
+}
