4f9e9cee0847ee1bf980ec2bf9e9cc464717bb64
Davisilveira <80595329+Unordestino@users.noreply.github.com>
2021-09-20 09:47:31 -0300

    Add files via upload

---
+++ 1180.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1180 {
+    class Program {
+        static void Main(string[] args) {
+            int.TryParse(Console.ReadLine(), out int N);
+            int[] X = new int[N];
+            int menor = 0, posicao = 0;
+
+            for (int i = 0; i < N; i++) {
+                int.TryParse(Console.ReadLine(), out X[i]);
+                if (i == 0) {
+                    menor = X[i];
+                    posicao = i;
+                }
+                if (X[i] < menor) {
+                    menor = X[i];
+                    posicao = i;
+                }
+            }
+            Console.WriteLine("Menor valor: " + menor);
+            Console.WriteLine("Posicao: " + posicao);
+        }
+    }
+}
