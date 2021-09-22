65921b87f7f90fb77201a50edc7adba90727b273
Davisilveira <80595329+Unordestino@users.noreply.github.com>
2021-09-22 06:51:33 -0300

    Add files via upload

---
+++ 1181.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _1181 {
+    class Program {
+        static void Main(string[] args) {
+
+            double[,] M = new double[12, 12];
+            int.TryParse(Console.ReadLine(), out int L);
+            char.TryParse(Console.ReadLine().ToUpper(), out char T);
+            double soma = 0;
+
+
+            for (int i = 0; i < 12; i++) {
+                for (int j = 0; j < 12; j++) {
+                    M[i, j] = double.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int j = 0; j < 12; j++) {
+                soma += M[L, j];
+            }
+
+            if (T == 'M') soma /= 12;
+
+             Console.WriteLine(soma.ToString("F1"));
+        }
+    }
+}
---
+++ 1182.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _1182 {
+    class Program {
+        static void Main(string[] args) {
+            int.TryParse(Console.ReadLine(), out int C);
+            double soma = 0;
+
+            char.TryParse(Console.ReadLine().ToUpper(), out char T);
+            double[,] M = new double[12, 12];
+
+            for (int i = 0; i <12; i++) {
+                for (int j = 0; j < 12; j++) {
+                    M[i, j] = double.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < 12; i++) {
+                soma += M[i, C];
+            }
+
+
+            if (T == 'M') soma /= 12;
+            Console.WriteLine(soma.ToString("F1"));
+
+        }
+    }
+}
