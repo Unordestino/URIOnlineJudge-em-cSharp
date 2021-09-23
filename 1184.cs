5e32a7cfdeb9642eaceaf3d2c447627f00119663
Davisilveira <80595329+Unordestino@users.noreply.github.com>
2021-09-22 23:33:45 -0300

    Add files via upload

---
+++ 1184.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _1184 {
+    class Program {
+        static void Main(string[] args) {
+            char.TryParse(Console.ReadLine().ToUpper(), out char T);
+            double soma = 0;
+            double[,] M = new double[12, 12];
+
+            for (int i = 0; i < 12; i++) {
+                for (int j = 0; j < 12; j++) {
+                    M[i, j] = double.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < 12; i++) {
+                for (int j = 0; j < 12; j++) {
+                    if (j < i) soma += M[i, j];
+                }
+            }
+
+
+            if (T == 'M') soma /= ((12 * 12) - 12) / 2;
+            Console.WriteLine(soma.ToString("F1"));
+
+        }
+    }
+}
